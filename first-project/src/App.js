5579e4903900607a48d9557f4088c8d1b8b1c88d
Vinaykumarkummarikuntla <kvk.9618@gmail.com>
2024-06-01 15:31:33 +0530

    multiple components

--- first-project/src/App.js
+++ first-project/src/App.js
@@ -2,13 +2,26 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  var x = 'Hi vinay kumar how are you!'
+  var y = 10
+  var z = 20
+
+  function dummy(x, y) {
+    return x+y
+    }
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
+          <br />
+          welcome {x}
         </p>
+        <br />
+        sum of two numbers {dummy(y,z)}
+
         <a
           className="App-link"
           href="https://reactjs.org"
---
+++ first-project/src/components.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import './components.css'
+
+export  function Components() {
+  return (
+    <div>
+      <h1>welcome to first component  page</h1>
+    </div>
+  )
+}
+
+
+
+export  function Component1() {
+  return (
+    <div>
+   <h2>welcome to second component  page </h2> 
+    </div>
+  )
+}
+
+
+export  function Component2() {
+    return (
+      <div>
+        <h1>welcome to third component page</h1>
+      </div>
+    )
+  }
